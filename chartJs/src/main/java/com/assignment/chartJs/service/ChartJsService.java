37bb79d35176b3c6aaff00dda804028546126fed
SONG-YEE <119464641+SONG-YEE@users.noreply.github.com>
2023-04-24 17:20:39 +0900

    2

--- chartJs/src/main/java/com/assignment/chartJs/service/ChartJsService.java
+++ chartJs/src/main/java/com/assignment/chartJs/service/ChartJsService.java
@@ -1,12 +1,17 @@
 package com.assignment.chartJs.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.assignment.chartJs.entity.Student;
 import com.assignment.chartJs.repository.LectureRepository;
@@ -24,9 +29,25 @@ public class ChartJsService {
         this.studentRepository = studentRepository;
     }
     
-    public List<Object[]> getStudentsByLevel(byte level) {
-        return studentRepository.getStudentsByLevel(level);
-    }
-    
-    
+//    public List<Student[]> getStudentsByLevel(byte level) {
+//    	System.out.println("안찍힘");
+//    	System.err.println("안찍힘");
+//        return studentRepository.getStudentsByLevel(level);
+//    }
+	
+	@GetMapping("/students")
+	public List<Student> selectStu() {
+		List<Student> stu = studentRepository.findAll();
+		return stu;
+	}
+	
+	
+//	public void testGetStudentByGrade() {
+//        List<Object[]> resultList = studentRepository.getStudentsByLevel((byte) 10);
+//        System.out.println("출력 안됨");
+//        for(Object[] row : resultList) {
+//        	
+//        }
+//    }
+	 
 }
