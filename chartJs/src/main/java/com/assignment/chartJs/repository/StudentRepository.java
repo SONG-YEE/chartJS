76b3f00f3162a71eb275cdfb36c7d56835cf50c5
SONG-YEE <119464641+SONG-YEE@users.noreply.github.com>
2023-04-24 17:20:14 +0900

    2

--- chartJs/src/main/java/com/assignment/chartJs/repository/StudentRepository.java
+++ chartJs/src/main/java/com/assignment/chartJs/repository/StudentRepository.java
@@ -11,22 +11,22 @@ import com.assignment.chartJs.entity.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer>{
 	
-//		@Query("SELECT name, gender, level, track, Lecture.title, Score.value " +
-//					    "FROM Student " +
-//					    "INNER JOIN Score ON Student.seq = Score.student " +
-//					    "INNER JOIN Lecture ON Score.lecture = Lecture.seq " +
-//					    "WHERE level = :level " +
-//					    "GROUP BY name, gender, level, track, Lecture.title, Score.value " +
-//					    "ORDER BY level, track")
-//			List<Object[]> getStudentsByLevel(@Param("level") byte level);
-	@Query(value = "SELECT s.name, s.gender, s.level, s.track, l.title, sc.value " +
-            "FROM student s " +
-            "INNER JOIN score sc ON s.seq = sc.student " +
-            "INNER JOIN lecture l ON sc.lecture = l.seq " +
-            "WHERE s.level = :level " +
-            "GROUP BY s.name, s.gender, s.level, s.track, l.title, sc.value " +
-            "ORDER BY s.level, s.track", nativeQuery = true)
-List<Object[]> getStudentsByLevel(@Param("level") byte level);
+		@Query(value = "SELECT name, gender, level, track, Lecture.title, Score.value " +
+					    "FROM Student " +
+					    "INNER JOIN Score ON Student.seq = Score.student " +
+					    "INNER JOIN Lecture ON Score.lecture = Lecture.seq " +
+					    "WHERE level = :level " +
+					    "GROUP BY name, gender, level, track, Lecture.title, Score.value " +
+					    "ORDER BY level, track", nativeQuery = true)
+			List<Student[]> getStudentsByLevel(@Param("level") byte level);
+//	@Query(value = "SELECT s.name, s.gender, s.level, s.track, l.title, sc.value " +
+//            "FROM student s " +
+//            "INNER JOIN score sc ON s.seq = sc.student " +
+//            "INNER JOIN lecture l ON sc.lecture = l.seq " +
+//            "WHERE s.level = :level " +
+//            "GROUP BY s.name, s.gender, s.level, s.track, l.title, sc.value " +
+//            "ORDER BY s.level, s.track", nativeQuery = true)
+//List<Object[]> getStudentsByLevel(@Param("level") byte level);
 }
 
 //@Repository
