8f266ba79f0d3e7315ef5dce7f1aa3a5e924dcca
SONG-YEE <119464641+SONG-YEE@users.noreply.github.com>
2023-04-26 23:13:41 +0900

    3

--- chartJs/src/main/java/com/assignment/chartJs/controller/ChartJsController.java
+++ chartJs/src/main/java/com/assignment/chartJs/controller/ChartJsController.java
@@ -1,72 +1,55 @@
 package com.assignment.chartJs.controller;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.assignment.chartJs.entity.Student;
+import com.assignment.chartJs.dto.StudentDTO;
 import com.assignment.chartJs.repository.StudentRepository;
 import com.assignment.chartJs.service.ChartJsService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
-@RequestMapping("/")
 public class ChartJsController {
-	
+
 	@Autowired
 	private ChartJsService chartJsService;
-	
-//	@Autowired
-//	public ChartJsController(ChartJsService chartJsService) {
-//      this.chartJsService = chartJsService;
-//	}
 	
 	@Autowired
 	private StudentRepository studentRepository;
 	
-//	@GetMapping
+//	@GetMapping("/index")
 //	public String chartView() {
 //		return "index";
 //	}
 	
-//	@GetMapping("/students")
-//	public List<Student[]> getStudents(@PathVariable byte level) {
-//		return studentRepository.getStudentsByLevel(level);
-//	}
-	
-
-//	@GetMapping(value = "/students/{level}", produces = "application/json")
-//	public List<Object[]> getStudents(@PathVariable byte level) {
-//	    return chartJsService.getStudentsByLevel(level);
-//	}
-	
-//	@GetMapping("/students")
-//    public List<Student> getStudents() {
-//        return studentRepository.findAll();
-//    }
-	
-	public List<Student> abc() {
-		return chartJsService.selectStu();
+	@GetMapping("/{level}")
+	public String getStudentsByLevel(@PathVariable byte level) {
+		
+		return chartJsService.getStudentsByLevel(level);
 	}
 	
-//	public String getStudents(Model model) {
-//		List<Object[]> students10 = chartJsService.getStudentsByLevel((byte)10);
-//		List<Object[]> students11 = chartJsService.getStudentsByLevel((byte)11);
-//		List<Object[]> students12 = chartJsService.getStudentsByLevel((byte)12);
-//        model.addAttribute("students11", students11);
-//        model.addAttribute("students12", students12);
-//		return "index";
-//	}
 	
-//	@GetMapping("/students/{level}")
-//	public Map<String, List<Object[]>> getStudents(@PathVariable byte level) {
-//		return (Map<String, List<Object[]>>) chartJsService.getStudentsByLevel(level);
+//	@GetMapping("/hello")
+//	public String getJson() {
+//		List<Object[]> st = getStudentsByLevel((byte) 10);
+//		
+//		
+//		for(Object[] s : st) {
+//			
+//			System.out.println("seq: " + st[0]);
+//		    System.out.println("name: " + st[1]);
+//		    System.out.println("gender: " + student[2]);
+//		    System.out.println("level: " + student[3]);
+//		    System.out.println("track: " + student[4]);
+//		    System.out.println("title: " + student[5]);
+//		    System.out.println("value: " + student[6]);
+//		    
+//		}
+//		return 
 //	}
 
 }
