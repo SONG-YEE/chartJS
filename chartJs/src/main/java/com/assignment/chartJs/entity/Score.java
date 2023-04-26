88d3f3830987434ce90df5bf99e83c0fa2b1cb3e
SONG-YEE <119464641+SONG-YEE@users.noreply.github.com>
2023-04-26 23:17:22 +0900

    3

--- chartJs/src/main/java/com/assignment/chartJs/entity/Score.java
+++ chartJs/src/main/java/com/assignment/chartJs/entity/Score.java
@@ -5,19 +5,24 @@ import lombok.*;
 
 
 @Builder
-@Data
-@Entity
-@Table(name="Score")
+@Getter
+@Setter
+@ToString
+@Entity(name = "Score")
 public class Score {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="seq")
 	private int seq;
 
+	@Column(name="value")
     private byte value;
 
+	@Column(name="student")
     private int student;
     
+	@Column(name="lecture")
     private int lecture;
 
 }
