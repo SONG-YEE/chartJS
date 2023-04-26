05091b29c37f57ccca35b8a7d36e9c8cf94f1647
SONG-YEE <119464641+SONG-YEE@users.noreply.github.com>
2023-04-26 23:17:43 +0900

    3

--- chartJs/src/main/java/com/assignment/chartJs/entity/Student.java
+++ chartJs/src/main/java/com/assignment/chartJs/entity/Student.java
@@ -5,22 +5,39 @@ import lombok.*;
 
 
 @Builder
-@Data
-@Entity
-@Table(name="Student")
+@Getter
+@Setter
+@ToString
+@Entity(name = "Student")
 public class Student {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="seq")
 	private int seq;
 
+	@Column(name="name")
     private String name;
 
     @Enumerated(EnumType.STRING)
+    @Column(name="gender")
     private GenderEnum genderEnum;
     
+    @Column(name="level")
     private byte level;
     
+    @Column(name="track")
     private byte track;
+
+    
+	@Override
+	public String toString() {
+		return "Student [seq=" + seq + ", name=" + name + ", genderEnum=" + genderEnum + ", level=" + level + ", track="
+				+ track + "]";
+	}
+    
+    
+    
+    
     
 }
